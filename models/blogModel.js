a30f214f751e6e6f6306e73fb1a368d83a488354
mritunjayRoy <mroy8202@gmail.com>
2024-07-02 16:39:11 +0530

    models added

---
+++ models/blogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+    blogTitle: {
+        type: String,
+        required: true
+    },
+    blogImage: {
+        type: String
+    },
+    blogContent: {
+        type: String,
+        required: true
+    },
+    blogPostedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    } 
+}, {timestamps: true});
+
+module.exports = mongoose.model("Blog", blogSchema);
---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String
+    },
+    userPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Blog"
+    }]
+});
+
+module.exports = mongoose.model("User", userSchema);
