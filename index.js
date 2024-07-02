2507475e66c6e2473735e98eff1e1009a9cb4b02
mritunjayRoy <mroy8202@gmail.com>
2024-07-02 16:35:16 +0530

    database connected

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
 
+const database = require("./config/database");
+
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
+// connect to database
+database.connect();
+
 // activate server
 app.listen(process.env.port, () => {
-    console.log(`App is running at port ${port}`)
-})
+    console.log(`App is running at port ${port}`);
+});
 
 // default route
 app.get("/", (req, res) => {
     return res.json({
         success: true,
         message: "Welcome to default route"
-    })
-})
+    });
+});
